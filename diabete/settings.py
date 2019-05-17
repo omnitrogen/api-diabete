0cde29bebcc650dd8069ceedea1bac4426e8a851
omnitrogen <felixgif@gmail.com>
2019-05-18 00:21:57 +0200

    bleh

--- diabete/settings.py
+++ diabete/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "$p_w9kypl&c%eir_9k3s-e1&smq=xmzsgsqvsfz3=(sa*#4-$c"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
     "api",
 ]
 
@@ -49,7 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
+    "app.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "diabete.urls"
@@ -115,7 +114,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
-
-# CORS
-CORS_ORIGIN_ALLOW_ALL = True
